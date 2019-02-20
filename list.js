b55cb712b4bb2c3c363343f6c8d74e654d0a68b4
raghdiaissa <raghdiaissa@gmail.com>
2019-02-20 23:05:01 +0100

    Update list.js

--- list.js
+++ list.js
@@ -2,42 +2,42 @@
 		 $(document).ready(function(){
 			 
 			$(".b1").click(function(){
-				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11466.m3u8');
+				player.load('http://51.15.21.98:8898/live/lkjsdlkjdsljsd/0f98593581/11466.m3u8');
 			});
 			
 			$(".b2").click(function(){
-				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11569.m3u8');
+				player.load('http://51.15.21.98:8898/live/lkjsdlkjdsljsd/0f98593581/11569.m3u8');
 			});
 			
 			$(".b3").click(function(){
-				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11570.m3u8');
+				player.load('http://51.15.21.98:8898/live/lkjsdlkjdsljsd/0f98593581/11570.m3u8');
 			});
 			
 			$(".b4").click(function(){
-				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11571.m3u8');
+				player.load('http://51.15.21.98:8898/live/lkjsdlkjdsljsd/0f98593581/11571.m3u8');
 			});
 			
 			$(".b5").click(function(){
-				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11572.m3u8');
+				player.load('http://51.15.21.98:8898/live/lkjsdlkjdsljsd/0f98593581/11572.m3u8');
 			});
 			
 			$(".b6").click(function(){
-				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11573.m3u8');
+				player.load('http://51.15.21.98:8898/live/lkjsdlkjdsljsd/0f98593581/11573.m3u8');
 			});
 			
 			$(".b7").click(function(){
-				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11574.m3u8');
+				player.load('http://51.15.21.98:8898/live/lkjsdlkjdsljsd/0f98593581/11574.m3u8');
 			});
 			
 			$(".b8").click(function(){
-				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11575.m3u8');
+				player.load('http://51.15.21.98:8898/live/lkjsdlkjdsljsd/0f98593581/11575.m3u8');
 			});
 			
 			$(".b9").click(function(){
-				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11576.m3u8');
+				player.load('http://51.15.21.98:8898/live/lkjsdlkjdsljsd/0f98593581/11576.m3u8');
 			});
 			
 			$(".b10").click(function(){
-				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11577.m3u8');
+				player.load('http://51.15.21.98:8898/live/lkjsdlkjdsljsd/0f98593581/11577.m3u8');
 			});
 		});
