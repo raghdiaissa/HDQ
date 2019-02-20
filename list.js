2a8f1b6cebf31d9c07307eea71737fa236ce576e
raghdiaissa <raghdiaissa@gmail.com>
2019-02-20 23:01:22 +0100

    Add files via upload

---
+++ list.js
@@ -0,0 +1,43 @@
+
+		 $(document).ready(function(){
+			 
+			$(".b1").click(function(){
+				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11466.m3u8');
+			});
+			
+			$(".b2").click(function(){
+				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11569.m3u8');
+			});
+			
+			$(".b3").click(function(){
+				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11570.m3u8');
+			});
+			
+			$(".b4").click(function(){
+				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11571.m3u8');
+			});
+			
+			$(".b5").click(function(){
+				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11572.m3u8');
+			});
+			
+			$(".b6").click(function(){
+				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11573.m3u8');
+			});
+			
+			$(".b7").click(function(){
+				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11574.m3u8');
+			});
+			
+			$(".b8").click(function(){
+				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11575.m3u8');
+			});
+			
+			$(".b9").click(function(){
+				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11576.m3u8');
+			});
+			
+			$(".b10").click(function(){
+				player.load('http://51.15.21.98:8898/live/eoeproprkf/5df00dafa2/11577.m3u8');
+			});
+		});
